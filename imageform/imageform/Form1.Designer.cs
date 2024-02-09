2f44c3f1100c2cc952300d1140c05a82a73db5bc
Zayed Hasan <95176649+Zayed53@users.noreply.github.com>
2024-02-09 21:57:26 +0600

    Add files via upload

--- imageform/imageform/Form1.Designer.cs
+++ imageform/imageform/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.load = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,9 +66,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(858, 277);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(134, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "change";
+            this.button1.Text = "change to negative";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -68,17 +76,105 @@
             // 
             this.button2.Location = new System.Drawing.Point(858, 49);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(134, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "change to grey";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(858, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "face detection";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(858, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "change to alpha tone";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(858, 165);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Rotate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(858, 216);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(134, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "flip";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(858, 323);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(134, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "increase brightness";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(858, 409);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "increase contrast";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(39, 28);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(165, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "change to poster";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(39, 88);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(165, 23);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "add paint effect";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1144, 489);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.load);
@@ -95,6 +191,14 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- imageform/imageform/Form1.cs
+++ imageform/imageform/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
 
 namespace imageform
 {
@@ -61,6 +67,149 @@ namespace imageform
                 }
             }
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> img = new Image<Bgr, byte>(path);
+
+
+            Image<Gray, byte> grayImage = img.Convert<Gray, byte>();
+
+
+            CascadeClassifier faceClassifier = new CascadeClassifier("C:/Users/User/Documents/haarcascade_frontalface_default.xml");
+
+
+            var faces = faceClassifier.DetectMultiScale(grayImage, 1.3, 5);
+
+            foreach (var face in faces)
+            {
+                img.Draw(face, new Bgr(Color.Red), 2);
+            }
+
+
+            pictureBox1.Image = img.ToBitmap();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(path);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color p = bmp.GetPixel(i, y);
+                    int tr = (int)(0.393 * p.R + 0.769 * p.G + 0.189 * p.B);
+                    int tg = (int)(0.349 * p.R + 0.686 * p.G + 0.168 * p.B);
+                    int tb = (int)(0.272 * p.R + 0.534 * p.G + 0.131 * p.B);
+                    tr = Math.Min(tr, 255);
+                    tg = Math.Min(tg, 255);
+                    tb = Math.Min(tb, 255);
+                    bmp.SetPixel(i, y, Color.FromArgb(tr, tg, tb));
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(path);
+            bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(path);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Image = bmp;
+        }
+
+        int brigntess = 0;
+        private void button7_Click(object sender, EventArgs e)
+        {
+            brigntess=brigntess+40;
+            Bitmap bmp = new Bitmap(path);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color p = bmp.GetPixel(i, y);
+                    int tr = (int)( p.R + brigntess);
+                    int tg = (int)( p.G + brigntess);
+                    int tb = (int)( p.B + brigntess);
+                    tr = Math.Min(tr, 255);
+                    tg = Math.Min(tg, 255);
+                    tb = Math.Min(tb, 255);
+                    bmp.SetPixel(i, y, Color.FromArgb(tr, tg, tb));
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        int contrast = 0;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            contrast = contrast + 40;
+            Bitmap bmp = new Bitmap(path);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color p = bmp.GetPixel(i, y);
+                    int tr = (int)((p.R * contrast)/255.0);
+                    int tg = (int)((p.G * contrast)/255.0);
+                    int tb = (int)((p.B * contrast)/255.0);
+                    tr = Math.Min(tr, 255);
+                    tg = Math.Min(tg, 255);
+                    tb = Math.Min(tb, 255);
+                    bmp.SetPixel(i, y, Color.FromArgb(tr, tg, tb));
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+        
+        int levels = 4;
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(path);
+            int stepSize = 255 / (levels - 1); // Calculate the step size
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color p = bmp.GetPixel(i, y);
+                    int r = (p.R / stepSize) * stepSize; // Round to the nearest step
+                    int g = (p.G / stepSize) * stepSize;
+                    int b = (p.B / stepSize) * stepSize;
+                    bmp.SetPixel(i, y, Color.FromArgb(r, g, b));
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(path);
+            Random rand = new Random();
+            int intensity = 50;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color p = bmp.GetPixel(i, y);
+                    int r = p.R + rand.Next(-intensity, intensity + 1); 
+                    int g = p.G + rand.Next(-intensity, intensity + 1);
+                    int b = p.B + rand.Next(-intensity, intensity + 1);
+
+                    r = Math.Max(0, Math.Min(r, 255));
+                    g = Math.Max(0, Math.Min(g, 255));
+                    b = Math.Max(0, Math.Min(b, 255));
+
+                    bmp.SetPixel(i, y, Color.FromArgb(r, g, b));
+                    pictureBox1.Image = bmp;
+                }
+            }
         }
     }
 }
